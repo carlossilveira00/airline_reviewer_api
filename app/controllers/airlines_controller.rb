eb70d07fef4690ec3af1400b9b669aaa09405ab7
Carlos Silveira <carlos.dt.cd@gmail.com>
2022-12-22 11:23:11 -0100

    Create Reviews Controller.

--- app/controllers/airlines_controller.rb
+++ app/controllers/airlines_controller.rb
@@ -1,49 +1,53 @@
-class AirlinesController < ApplicationController
-  def index
-    @airlines = Airline.all
+module Api
+  module V1
+    class AirlinesController < ApplicationController
+      def index
+        @airlines = Airline.all
 
-    render json: @airlines
-  end
+        render json: @airlines
+      end
 
-  def show
-    @airline = Airline.findy_by(slug: params[:slug])
+      def show
+        @airline = Airline.findy_by(slug: params[:slug])
 
-    render json: @airline
-  end
+        render json: @airline
+      end
 
-  def create
-    @airline = Arline.new(airline_params)
+      def create
+        @airline = Arline.new(airline_params)
 
-    if @airline.save
-      render json: @airline
-    else
-      render json: { error: @airline.errors.messages }, status: 422
+        if @airline.save
+          render json: @airline
+        else
+          render json: { error: @airline.errors.messages }, status: 422
+        end
+      end
+
+      def update
+        @airline = Airline.findy_by(slug: params[:slug])
+
+        if @airline.update(airline_params)
+          render json: @airline
+        else
+          render json: { error: @airline.errors.messages }, status: 422
+        end
+      end
+
+      def destroy
+        @airline = Airline.findy_by(slug: params[:slug])
+
+        if @airline.destroy
+          head :no_content
+        else
+          render json: { error: @airline.errors.messages }, status: 422
+        end
+      end
+
+      private
+
+      def airline_params
+        params.require(:airline).permit(:name, :image_url)
+      end
     end
-  end
-
-  def update
-    @airline = Airline.findy_by(slug: params[:slug])
-
-    if @airline.update(airline_params)
-      render json: @airline
-    else
-      render json: { error: @airline.errors.messages }, status: 422
-    end
-  end
-
-  def destroy
-    @airline = Airline.findy_by(slug: params[:slug])
-
-    if @airline.destroy
-      head :no_content
-    else
-      render json: { error: @airline.errors.messages }, status: 422
-    end
-  end
-
-  private
-
-  def airline_params
-    params.require(:airline).permit(:name, :image_url)
   end
 end
